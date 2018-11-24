2d75f2125c23921b2c017dd3ce246cb20ec095dc
Jaka Stavanja <jakastavanja@gmail.com>
2018-11-24 21:38:01 +0100

    added kumuluzee service discovery module :mag_right:

--- src/index.js
+++ src/index.js
@@ -5,6 +5,27 @@ import express from 'express'
 import cors from 'cors'
 import morgan from 'morgan'
 import bodyParser from 'body-parser'
+import path from 'path'
+
+const configurationPath = path.join(__dirname, 'config', 'config.yaml')
+const KumuluzeeDiscovery = require('@kumuluz/kumuluzee-discovery').default
+const configurationUtil = require('@kumuluz/kumuluzee-config').ConfigurationUtil
+
+let util = null
+
+const register = async () => {
+
+    await configurationUtil.initialize({
+        extension: 'etcd',
+        configPath: configurationPath
+    })
+
+    util = configurationUtil
+    await KumuluzeeDiscovery.initialize({extension: 'etcd'})
+    KumuluzeeDiscovery.registerService()
+}
+
+register()
 
 let app = express()
 app.server = http.createServer(app)
