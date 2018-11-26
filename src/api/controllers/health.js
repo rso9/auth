015d31d0babc8ac322a8843f98e68bdd6292463d
Jaka Stavanja <jakastavanja@gmail.com>
2018-11-26 10:20:41 +0100

    added meaningful health checks (db, diskspace), v0.5

--- src/api/controllers/health.js
+++ src/api/controllers/health.js
@@ -1,8 +1,70 @@
+import diskspace from 'diskspace'
+import db from '../db'
+import User from '../models/User'
 const KumuluzeeDiscovery = require('@kumuluz/kumuluzee-discovery').default
 
-const health = (req, res) => {
-  res.json({status: 'UP'})
-  res.end()
+const checkDBHealth = () => {
+  return new Promise((resolve, reject) => {
+    let retval = {
+      "name": "DataSourceHealthCheck",
+      "state": "DOWN"
+    }
+    User.findAll().then((users) => {
+      if (users) {
+        retval.state = 'UP'
+        resolve(retval)
+      }
+    }).catch((err) => {
+      reject(retval)
+    })
+  })
+}
+
+const checkDiskspaceHealth = () => {
+  return new Promise((resolve, reject) => {
+    diskspace.check('/', (err, result) => {
+      console.log("total:", result.total)
+      console.log("free", result.free)
+
+      if (err) reject({
+        name: "DiskSpaceHealthCheck",
+        state: "DOWN"
+      })
+      
+      resolve({
+        name: "DiskSpaceHealthCheck",
+        state: (result.free > 0.01 * result.total) ? "UP" : "DOWN"
+      })
+    })
+  })
+}
+
+const health = async (req, res) => {
+  let health = {
+    state: 'DOWN',
+    checks: []
+  }
+
+  try {
+    const dbHealth = await checkDBHealth()
+    health.checks.push(dbHealth)
+  } catch (err) {
+    console.log(err)
+    health.checks.push(err)
+  }
+
+  try {
+    const diskpaceHealth = await checkDiskspaceHealth()
+    health.checks.push(diskpaceHealth)
+  } catch (err) {
+    health.checks.push(err)
+  }
+
+  let allGood = health.checks.every((e) => e.state === 'UP')
+  
+  if (allGood) health.state = 'UP'
+
+  res.status(allGood ? 200 : 500).json(health)
 }
 
 const lookup = async (req, res) => {
