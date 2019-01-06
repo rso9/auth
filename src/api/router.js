678fb70884ef785e4313eea395b8c3223b5bbc28
Jaka Stavanja <jakastavanja@gmail.com>
2019-01-06 17:50:54 +0100

    Added metrics endpoint

--- src/api/router.js
+++ src/api/router.js
@@ -2,6 +2,7 @@ import express from 'express'
 import userController from './controllers/user'
 import healthController from './controllers/health'
 import samplesController from './controllers/samples'
+import os from 'os'
 
 const router = express.Router()
 
@@ -15,5 +16,14 @@ router.get('/health', healthController.health)
 router.get('/lookup', healthController.lookup)
 router.get('/request_catalog', samplesController.requestCatalog)
 router.get('/v1/info', samplesController.projectInfo)
+
+router.get('/metrics', (req, res) => {
+  res.status(200).json({
+    cpuInfo: os.cpus(),
+    cpuUsage: os.loadavg(),
+    memoryUsage: os.freemem() / os.totalmem(),
+    uptime: os.uptime()
+  })
+})
 
 export default router
